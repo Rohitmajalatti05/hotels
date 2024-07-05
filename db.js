a464b82c540f657226a341e8ad3f1b57dcac7de7
Rohitmajalatti05 <141742203+Rohitmajalatti05@users.noreply.github.com>
2024-07-05 12:13:17 +0530

    shift mongodb to online server and added env file

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 const { debuglog } = require('util');
+require('dotenv').config(); //Importing env file
 
 //define the mongodb connection URL
 
-const mongoURL = 'mongodb://localhost:27017/hotels' 
+// const mongoURL = 'mongodb://localhost:27017/hotels' This is local database mongoDB
+const mongoURL = process.env.MONGODB_URL //This is MongoDb Atlas Server link getting from the env file
 
 //set up the mongodb connection
 mongoose.connect(mongoURL,{
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require('dotenv').config(); //Importing dotenv file 
 
 //Import Body Parser
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000; //accessing env file PORT variable
 
 app.get("/", (req, res) => {
     res.send("Welcome to rohits kitchen");
@@ -18,6 +20,7 @@ const menuRoutes = require('./routes/menuRoutes');
 // Use the Routers
 app.use('/person',personRoutes);
 app.use('/menu',menuRoutes);
+
 
 app.listen(3000, () => {
     console.log("Server listening on 3000");
